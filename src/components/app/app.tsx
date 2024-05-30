5e751a965f4237283c8ff6be19a975746e3dbcaf
Igor Semenov <igor.nevski.25@gmail.com>
2024-05-30 15:36:28 +0300

    type

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,76 @@
-import { ConstructorPage } from '@pages';
+import {
+  Feed,
+  ConstructorPage,
+  NotFound404,
+  Register,
+  Login,
+  ResetPassword,
+  Profile,
+  ProfileOrders
+} from '@pages';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader, Modal, OrderInfo, IngredientDetails } from '@components';
+
+const RouteApp = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const onClose = () => {
+    navigate(-1);
+  };
+
+  const background = location.state && location.state.background;
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/forgot-password' element={<ResetPassword />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/profile/orders' element={<ProfileOrders />} />
+        <Route path='*' element={<NotFound404 />} />
+      </Routes>
+      {background && (
+        <Routes>
+          <Route
+            path='/feed/:number'
+            element={
+              <Modal title='Заказы' onClose={onClose}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal title='Ингредиенты' onClose={onClose}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          <Route
+            path='/profile/orders/:number'
+            element={
+              <Modal title='Заказы' onClose={onClose}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+        </Routes>
+      )}
+    </>
+  );
+};
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <RouteApp />
   </div>
 );
 
---
+++ src/services/ProtectedRoute.tsx
@@ -0,0 +1,30 @@
+import { useSelector } from '../services/store';
+import { Navigate, useLocation, Outlet } from 'react-router';
+
+type ProtectedRouteProps = {
+  onlyUnAuth?: boolean;
+  children: React.ReactElement;
+};
+
+export const ProtectedRoute = ({
+  onlyUnAuth,
+  children
+}: ProtectedRouteProps) => {
+  const location = useLocation();
+  const { user, isAuthChecked } = useSelector((store) => store.user);
+
+  if (!isAuthChecked && !user) {
+    return <div>Loading...</div>;
+  }
+
+  if (!onlyUnAuth && !isAuthChecked) {
+    return <Navigate replace to='/login' state={{ from: location }} />;
+  }
+
+  if (onlyUnAuth && isAuthChecked) {
+    const from = location.state?.from || { pathname: '/' };
+    return <Navigate replace to={from} />;
+  }
+
+  return <Outlet />;
+};
